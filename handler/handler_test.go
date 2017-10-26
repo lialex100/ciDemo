04a4218a425c8589a6db259576ac9c76de66c112
chiu.siu@rocketdt.com <wachiu@wachius-MacBook-Pro.local>
2017-10-26 17:08:32 +0800

    test

---
+++ handler/handler_test.go
@@ -0,0 +1,14 @@
+package handler
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetRoot(t *testing.T) {
+	fmt.Println("TestGetRoot - Pass")
+}
+
+func TestProductList(t *testing.T) {
+	fmt.Println("TestProductList - Pass")
+}
